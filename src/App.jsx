9c7a2ae023f8ab3c8f2b1c2cc0917335c0d83b22
unknown <rasmitha18@users.noreply.github.com>
2026-04-20 12:10:59 +0530

    changed alphabet

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   const generateString = useCallback(() => {
     const chars =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+      "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
     let result = "";
 
     for (let i = 0; i < length; i++) {
